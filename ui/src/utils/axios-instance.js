671dcf34b8462007d2a1bf00c29a3b593516f58b
tanertasim <taner@camv.io>
2021-03-11 08:30:24 +0200

    Remove unused code from ui

--- ui/src/utils/axios-instance.js
+++ ui/src/utils/axios-instance.js
@@ -2,20 +2,6 @@ import axios from 'axios';
 import {API_ROOT} from './api-config';
 
 const axiosInstance = axios.create({
-	paramsSerializer(params) {
-		const searchParams = new URLSearchParams();
-		for (const key of Object.keys(params)) {
-			const param = params[key];
-			if (Array.isArray(param)) {
-				for (const p of param) {
-					searchParams.append(key, p);
-				}
-			} else {
-				searchParams.append(key, param);
-			}
-		}
-		return searchParams.toString();
-	},
 	baseURL: `${API_ROOT}`,
 	timeout: 5 * 60 * 1000,
 	headers: {
